cc105081d56780adf330740530fced570cb4fccf
Eric Poe <eric.poe@gmail.com>
2019-01-25 21:44:54 -0600

    Tag with a release

--- source/conf.py
+++ source/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, CoderDojoKC'
 author = 'CoderDojoKC'
 
 # The short X.Y version
-version = ''
+version = '0.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.0.1 alpha'
 
 
 # -- General configuration ---------------------------------------------------
